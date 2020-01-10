671077a17d0bfd2532cd09fc25e36cf3568d024a
Tac Tacelosky <tacman@gmail.com>
2020-01-10 10:29:18 -0500

    re-add js/css that were overwritten somehow.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,18 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+const $ = require('jquery');
+require('bootstrap');
+const Popper = require('popper.js');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+/**
+ \u0040fortawesome\/fontawesome\u002Dfree
+ bootstrap
+ fontawesome
+ jquery
+ popper.js
+ **/
