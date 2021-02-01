3b2132a9abad56e6025e2854e508331c7524779c
Tac Tacelosky <tacman@gmail.com>
2021-02-01 14:04:03 -0500

    get bootstrap working again

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
+// make sure $ and jquery are global
+const $ = require('jquery');
+global.jQuery = global.$ = $;
+require('popper.js');
 
-const jQuery = require('jquery');
+require('admin-lte'); // This comes from yarn add admin-lte (not the admin-lte bundle, which includes bootstrap).
+require('bootstrap');
+require('Hinclude/hinclude');
+require('jquery-sparkline');
+
+require('./styles/app.scss');
 
 // this is the node module, installed with yarn add admin-lte@^3.0
-require('admin-lte');
-
-require('bootstrap');
-
 const Popper = require('popper.js');
 
 // any CSS you require will output into a single css file (app.css in this case)
